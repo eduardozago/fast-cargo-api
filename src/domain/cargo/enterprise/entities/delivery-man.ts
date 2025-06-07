2ff091eaa89a7bfd1a19a602e725b4d7a627f454
Eduardo Zago <zagoe22@gmail.com>
2025-06-07 20:44:32 -0300

    feat: add getter methods on delivery man entity

--- src/domain/cargo/enterprise/entities/delivery-man.ts
+++ src/domain/cargo/enterprise/entities/delivery-man.ts
@@ -1,14 +1,30 @@
-import { Entity } from '../../../../core/entities/entity'
-import { UniqueEntityID } from '../../../../core/entities/unique-entity-id'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 export interface DeliveryManProps {
-  orderId: UniqueEntityID
+  orderId?: UniqueEntityID
   taxId: string
   name: string
   password: string
 }
 
 export class DeliveryMan extends Entity<DeliveryManProps> {
+  get orderId() {
+    return this.props.orderId
+  }
+
+  get taxId() {
+    return this.props.taxId
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(props: DeliveryManProps, id?: UniqueEntityID) {
     const deliverMan = new DeliveryMan(props, id)
 
