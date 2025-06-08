7324577d359e3c2e1b62766508afe06cceeb4c6a
Eduardo Zago <zagoe22@gmail.com>
2025-06-08 20:18:13 -0300

    test: change delivery man to delivery drive on factory

--- test/factories/make-delivery-driver.ts
+++ test/factories/make-delivery-driver.ts
@@ -1,15 +1,15 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
-  DeliveryMan,
-  DeliveryManProps,
-} from '@/domain/cargo/enterprise/entities/delivery-man'
+  DeliveryDriver,
+  DeliveryDriverProps,
+} from '@/domain/cargo/enterprise/entities/delivery-driver'
 import { faker } from '@faker-js/faker'
 
-export function makeDeliveryMan(
-  override: Partial<DeliveryManProps> = {},
+export function makeDeliveryDriver(
+  override: Partial<DeliveryDriverProps> = {},
   id?: UniqueEntityID,
 ) {
-  const deliverMan = DeliveryMan.create(
+  const deliverDriver = DeliveryDriver.create(
     {
       taxId: faker.string.numeric(),
       name: faker.person.fullName(),
@@ -19,5 +19,5 @@ export function makeDeliveryMan(
     id,
   )
 
-  return deliverMan
+  return deliverDriver
 }
