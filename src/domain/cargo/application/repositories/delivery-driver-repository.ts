7c325a81f59ad35404ab4f87e28ca87c7a26cd0c
Eduardo Zago <zagoe22@gmail.com>
2025-06-09 14:49:14 -0300

    feat: add find all method to delivery drivers repository

--- src/domain/cargo/application/repositories/delivery-driver-repository.ts
+++ src/domain/cargo/application/repositories/delivery-driver-repository.ts
@@ -1,8 +1,10 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { DeliveryDriver } from '../../enterprise/entities/delivery-driver'
 
 export abstract class DeliveryDriverRepository {
   abstract findById(id: string): Promise<DeliveryDriver | null>
   abstract findByTaxId(taxId: string): Promise<DeliveryDriver | null>
+  abstract findAll(params: PaginationParams): Promise<DeliveryDriver[]>
   abstract create(deliverDriver: DeliveryDriver): Promise<void>
   abstract save(deliverDriver: DeliveryDriver): Promise<void>
   abstract delete(deliverDriver: DeliveryDriver): Promise<void>
