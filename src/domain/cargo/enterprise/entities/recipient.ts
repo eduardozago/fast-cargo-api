74c005ef8d58763679af4b1670795450c962996b
Eduardo Zago <zagoe22@gmail.com>
2025-06-09 16:25:22 -0300

    feat: add getter methods to recipient entity

--- src/domain/cargo/enterprise/entities/recipient.ts
+++ src/domain/cargo/enterprise/entities/recipient.ts
@@ -1,12 +1,25 @@
-import { Entity } from '../../../../core/entities/entity'
-import { UniqueEntityID } from '../../../../core/entities/unique-entity-id'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 export interface RecipientProps {
   orderId: UniqueEntityID
   name: string
+  address: string
 }
 
 export class Recipient extends Entity<RecipientProps> {
+  get orderId() {
+    return this.props.orderId
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get address() {
+    return this.props.address
+  }
+
   static create(props: RecipientProps, id?: UniqueEntityID) {
     const recipient = new Recipient(props, id)
 
