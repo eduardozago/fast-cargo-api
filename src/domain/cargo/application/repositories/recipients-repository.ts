ed1891f840a5aa709e553fa6ebe68413dea79d22
Eduardo Zago <zagoe22@gmail.com>
2025-06-10 13:57:25 -0300

    feat: add save method on recipients repository

--- src/domain/cargo/application/repositories/recipients-repository.ts
+++ src/domain/cargo/application/repositories/recipients-repository.ts
@@ -3,4 +3,5 @@ import { Recipient } from '../../enterprise/entities/recipient'
 export abstract class RecipientsRepository {
   abstract findById(id: string): Promise<Recipient | null>
   abstract create(recipient: Recipient): Promise<void>
+  abstract save(recipient: Recipient): Promise<void>
 }
