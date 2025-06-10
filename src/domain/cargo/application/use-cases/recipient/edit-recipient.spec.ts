c7ff30abdb872295b67b106afcd6d2b16d65bdbb
Eduardo Zago <zagoe22@gmail.com>
2025-06-10 14:34:41 -0300

    test: add test to edit recipient use case with user role

--- src/domain/cargo/application/use-cases/recipient/edit-recipient.spec.ts
+++ src/domain/cargo/application/use-cases/recipient/edit-recipient.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryRecipientRepository } from 'test/respositories/in-memory-recipients-repository'
 import { EditRecipientUseCase } from './edit-recipient'
 import { makeRecipient } from 'test/factories/make-recipient'
+import { NotAllowedError } from '../errors/not-allowed-error'
 
 let inMemoryRecipientRepository: InMemoryRecipientRepository
 let sut: EditRecipientUseCase
@@ -24,9 +25,29 @@ describe('Edit Recipient', () => {
       recipientId: recipient.id.toString(),
       name: 'John Doe',
       address: 'Edited address',
+      role: 'ADMIN',
     })
 
     expect(result.isRight()).toBe(true)
     expect(result.value?.recipient.address).toEqual('Edited address')
+  })
+
+  it('should not be allowed to edit a recipient if the user is not admin', async () => {
+    const recipient = makeRecipient({
+      name: 'John Doe',
+      address: 'Address 1',
+    })
+
+    inMemoryRecipientRepository.create(recipient)
+
+    const result = await sut.execute({
+      recipientId: recipient.id.toString(),
+      name: 'John Doe',
+      address: 'Edited address',
+      role: 'DELIVER_DRIVER',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
