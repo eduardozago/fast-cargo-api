a0fdfc081646aa51945cd0d92191a03ed86eb492
Eduardo Zago <zagoe22@gmail.com>
2025-06-10 14:37:11 -0300

    feat: add user role validation to delete recipient use case

--- src/domain/cargo/application/use-cases/recipient/delete-recipient.ts
+++ src/domain/cargo/application/use-cases/recipient/delete-recipient.ts
@@ -1,19 +1,38 @@
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from '../errors/resource-not-found-error'
 import { RecipientsRepository } from '../../repositories/recipients-repository'
+import { UserRole } from '@/domain/cargo/enterprise/entities/user-role'
+import { NotAllowedError } from '../errors/not-allowed-error'
 
 interface DeleteRecipientUseCaseRequest {
   recipientId: string
+  role: string
 }
 
-type DeleteRecipientUseCaseResponse = Either<ResourceNotFoundError, null>
+type DeleteRecipientUseCaseResponse = Either<
+  NotAllowedError | ResourceNotFoundError,
+  null
+>
 
 export class DeleteRecipientUseCase {
   constructor(private recipientRepository: RecipientsRepository) {}
 
   async execute({
     recipientId,
+    role,
   }: DeleteRecipientUseCaseRequest): Promise<DeleteRecipientUseCaseResponse> {
+    const isValidRole = Object.values(UserRole).includes(role as UserRole)
+
+    if (!isValidRole) {
+      return left(new NotAllowedError())
+    }
+
+    const validRole = role as UserRole
+
+    if (validRole !== UserRole.ADMIN) {
+      return left(new NotAllowedError())
+    }
+
     const recipient = await this.recipientRepository.findById(recipientId)
 
     if (!recipient) {
