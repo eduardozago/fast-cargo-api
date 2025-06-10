f3d0c0cf754d50be091452f800b1948006cca25c
Eduardo Zago <zagoe22@gmail.com>
2025-06-10 14:08:51 -0300

    feat: add delete method to recipients repository

--- src/domain/cargo/application/repositories/recipients-repository.ts
+++ src/domain/cargo/application/repositories/recipients-repository.ts
@@ -4,4 +4,5 @@ export abstract class RecipientsRepository {
   abstract findById(id: string): Promise<Recipient | null>
   abstract create(recipient: Recipient): Promise<void>
   abstract save(recipient: Recipient): Promise<void>
+  abstract delete(recipient: Recipient): Promise<void>
 }
