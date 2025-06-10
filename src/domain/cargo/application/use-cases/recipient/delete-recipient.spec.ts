6abb1e1068f764d24d372850857384905fe39726
Eduardo Zago <zagoe22@gmail.com>
2025-06-10 14:37:32 -0300

    test: add test to delete recipient use case with user role

--- src/domain/cargo/application/use-cases/recipient/delete-recipient.spec.ts
+++ src/domain/cargo/application/use-cases/recipient/delete-recipient.spec.ts
@@ -2,6 +2,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryRecipientRepository } from 'test/respositories/in-memory-recipients-repository'
 import { DeleteRecipientUseCase } from './delete-recipient'
 import { makeRecipient } from 'test/factories/make-recipient'
+import { NotAllowedError } from '../errors/not-allowed-error'
 
 let inMemoryRecipientRepository: InMemoryRecipientRepository
 let sut: DeleteRecipientUseCase
@@ -25,9 +26,24 @@ describe('Delete Recipient', () => {
 
     const result = await sut.execute({
       recipientId: 'driver-01',
+      role: 'ADMIN',
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryRecipientRepository.items.length).toEqual(0)
+  })
+
+  it('should not be allowed to delete a recipient if the user is not admin', async () => {
+    const recipient = makeRecipient()
+
+    inMemoryRecipientRepository.create(recipient)
+
+    const result = await sut.execute({
+      recipientId: recipient.id.toString(),
+      role: 'DELIVER_DRIVER',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
