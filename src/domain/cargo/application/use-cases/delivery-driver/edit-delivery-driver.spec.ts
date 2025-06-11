913d4741b3a8c6df1a78ea18220d907d313469a8
Eduardo Zago <zagoe22@gmail.com>
2025-06-11 17:16:49 -0300

    test: update test on edit delivery driver use case with user role validation

--- src/domain/cargo/application/use-cases/delivery-driver/edit-delivery-driver.spec.ts
+++ src/domain/cargo/application/use-cases/delivery-driver/edit-delivery-driver.spec.ts
@@ -1,6 +1,8 @@
 import { InMemoryDeliveryDriverRepository } from 'test/respositories/in-memory-delivery-driver-repository'
 import { makeDeliveryDriver } from 'test/factories/make-delivery-driver'
 import { EditDeliveryDriverUseCase } from './edit-delivery-driver'
+import { makeAdministrator } from 'test/factories/make-administrator'
+import { NotAllowedError } from '../errors/not-allowed-error'
 
 let inMemoryDeliveryDriverRepository: InMemoryDeliveryDriverRepository
 let sut: EditDeliveryDriverUseCase
@@ -18,15 +20,37 @@ describe('Edit Delivery Driver', () => {
       password: '123456',
     })
 
+    const administrator = makeAdministrator()
+
     inMemoryDeliveryDriverRepository.create(deliveryDriver)
 
     const result = await sut.execute({
       deliveryDriverId: deliveryDriver.id.toString(),
       name: 'John Doe',
       password: '123123',
+      role: administrator.role,
     })
 
     expect(result.isRight()).toBe(true)
     expect(result.value?.deliveryDriver.password).toEqual('123123')
+  })
+
+  it('should not be able to edit a delivery driver if the use is not an administrator', async () => {
+    const deliveryDriver = makeDeliveryDriver({
+      name: 'John Doe',
+      password: '123456',
+    })
+
+    inMemoryDeliveryDriverRepository.create(deliveryDriver)
+
+    const result = await sut.execute({
+      deliveryDriverId: deliveryDriver.id.toString(),
+      name: 'John Doe',
+      password: '123123',
+      role: deliveryDriver.role,
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
