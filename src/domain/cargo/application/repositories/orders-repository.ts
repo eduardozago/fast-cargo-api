8e376903605edfdcb9124c3d1a45eb12c0bc5f60
Eduardo Zago <zagoe22@gmail.com>
2025-06-12 17:11:27 -0300

    feat: add create method to orders repository

--- src/domain/cargo/application/repositories/orders-repository.ts
+++ src/domain/cargo/application/repositories/orders-repository.ts
@@ -2,4 +2,5 @@ import { Order } from '../../enterprise/entities/order'
 
 export abstract class OrdersRepository {
   abstract findById(id: string): Promise<Order | null>
+  abstract create(order: Order): Promise<void>
 }
