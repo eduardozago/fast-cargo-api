7cce1e0d0053a05126b2c0c1746cab53f0aec4b4
Eduardo Zago <zagoe22@gmail.com>
2025-06-14 16:01:09 -0300

    feat: add expected order status parameter to invalid order status error

--- src/domain/cargo/application/use-cases/errors/invalid-order-status-error.ts
+++ src/domain/cargo/application/use-cases/errors/invalid-order-status-error.ts
@@ -2,9 +2,12 @@ import { UseCaseError } from '@/core/errors/use-case-error'
 import { OrderStatus } from '@/domain/cargo/enterprise/entities/order'
 
 export class InvalidOrderStatusError extends Error implements UseCaseError {
-  constructor(currentOrderStatus: OrderStatus) {
+  constructor(
+    currentOrderStatus: OrderStatus,
+    expectedOrderStatus: OrderStatus,
+  ) {
     super(
-      `Invalid order status. Current status is "${currentOrderStatus}" expected "${OrderStatus.ADDED}"`,
+      `Invalid order status. Current status is "${currentOrderStatus}" expected "${expectedOrderStatus}"`,
     )
   }
 }
