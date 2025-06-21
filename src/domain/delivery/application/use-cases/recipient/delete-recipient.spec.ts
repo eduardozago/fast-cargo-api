83df0a53aacbd92992e9d0dd27944ee61ef6df73
Eduardo Zago <zagoe22@gmail.com>
2025-06-21 18:52:52 -0300

    test: refactor delete recipient use case test

--- src/domain/delivery/application/use-cases/recipient/delete-recipient.spec.ts
+++ src/domain/delivery/application/use-cases/recipient/delete-recipient.spec.ts
@@ -2,7 +2,6 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryRecipientRepository } from 'test/respositories/in-memory-recipients-repository'
 import { DeleteRecipientUseCase } from './delete-recipient'
 import { makeRecipient } from 'test/factories/make-recipient'
-import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 
 let inMemoryRecipientRepository: InMemoryRecipientRepository
 let sut: DeleteRecipientUseCase
@@ -26,24 +25,9 @@ describe('Delete Recipient', () => {
 
     const result = await sut.execute({
       recipientId: 'driver-01',
-      role: 'ADMIN',
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryRecipientRepository.items.length).toEqual(0)
-  })
-
-  it('should not be allowed to delete a recipient if the user is not admin', async () => {
-    const recipient = makeRecipient()
-
-    inMemoryRecipientRepository.create(recipient)
-
-    const result = await sut.execute({
-      recipientId: recipient.id.toString(),
-      role: 'DELIVER_DRIVER',
-    })
-
-    expect(result.isLeft()).toBe(true)
-    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
