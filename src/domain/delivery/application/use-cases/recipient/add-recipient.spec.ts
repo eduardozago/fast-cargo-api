c4ceee9e14afa177bdc9c0b557bc49bbc9036cee
Eduardo Zago <zagoe22@gmail.com>
2025-06-21 18:51:04 -0300

    test: refactor add recipient use case test

--- src/domain/delivery/application/use-cases/recipient/add-recipient.spec.ts
+++ src/domain/delivery/application/use-cases/recipient/add-recipient.spec.ts
@@ -19,24 +19,10 @@ describe('Add Recipient', () => {
     const result = await sut.execute({
       name: recipient.name,
       address: recipient.address,
-      role: 'ADMIN',
     })
 
     expect(result.isRight()).toBe(true)
     expect(result.value?.recipient.name).toEqual(recipient.name)
     expect(inMemoryRecipientRepository.items).toHaveLength(1)
-  })
-
-  it('should not be allowed to add a recipient if the user is not admin', async () => {
-    const recipient = makeRecipient()
-
-    const result = await sut.execute({
-      name: recipient.name,
-      address: recipient.address,
-      role: 'DELIVERY_DRIVER',
-    })
-
-    expect(result.isLeft()).toBe(true)
-    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
