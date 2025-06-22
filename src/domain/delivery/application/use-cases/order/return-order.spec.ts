28060f2e489deaaa511e56cb6bfe75ddc2384f2f
Eduardo Zago <zagoe22@gmail.com>
2025-06-22 19:44:48 -0300

    test: remove role checks from return order use case tests

--- src/domain/delivery/application/use-cases/order/return-order.spec.ts
+++ src/domain/delivery/application/use-cases/order/return-order.spec.ts
@@ -1,6 +1,5 @@
 import { InMemoryOrdersRepository } from 'test/respositories/in-memory-orders-repository'
 import { makeOrder } from 'test/factories/make-order'
-import { UnauthorizedError } from '@/core/errors/errors/unauthorized-error'
 import { OrderStatus } from '@/domain/delivery/enterprise/entities/order'
 import { InvalidOrderStatusError } from '../errors/invalid-order-status-error'
 import { ReturnOrderUseCase } from './return-order'
@@ -24,27 +23,10 @@ describe('Return Order', () => {
 
     const result = await sut.execute({
       orderId: order.id.toString(),
-      role: 'ADMIN',
     })
-
-    console.log(result)
 
     expect(result.isRight()).toBe(true)
     expect(result.value?.order.id).toEqual(order.id)
-  })
-
-  it('should not authorized to return an order if the user is not admin', async () => {
-    const order = makeOrder()
-
-    inMemoryOrdersRepository.items.push(order)
-
-    const result = await sut.execute({
-      orderId: order.id.toString(),
-      role: 'DELIVERY_DRIVER',
-    })
-
-    expect(result.isLeft()).toBe(true)
-    expect(result.value).toBeInstanceOf(UnauthorizedError)
   })
 
   it('should not able to return an order if the status order is not "PICKED_UP"', async () => {
@@ -56,7 +38,6 @@ describe('Return Order', () => {
 
     const result = await sut.execute({
       orderId: order.id.toString(),
-      role: 'ADMIN',
     })
 
     expect(result.isLeft()).toBe(true)
