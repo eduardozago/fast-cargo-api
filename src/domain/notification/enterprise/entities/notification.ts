358a178cd0ef91d64e48c0b6e1b7f8155948dafa
Eduardo Zago <zagoe22@gmail.com>
2025-06-28 19:19:58 -0300

    feat: implement notification entity

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,52 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+import { Entity } from '@/core/entities/entity'
+
+export interface NotificationProps {
+  recipientId: UniqueEntityID
+  title: string
+  content: string
+  readedAt?: Date
+  createdAt: Date
+}
+
+export class Notification extends Entity<NotificationProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get readedAt() {
+    return this.props.readedAt
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  read() {
+    this.props.readedAt = new Date()
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return notification
+  }
+}
